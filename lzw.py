6f0693a17f7642578edb311548b93570908a7e8e
parth_shel <ps@parthshel.com>
2021-08-22 15:40:23 -0400

    fixed compression algorithm and implemented decompression algorithm

--- lzw.py
+++ lzw.py
@@ -1,26 +1,48 @@
 # takes text string and return compressed string
 def compress(text):
     split_text = list(text)
-    print(split_text)
     
-    result = ''
+    result = []
     string = ''
-    dictionary = {}
+    dictionary_size = 256
+    dictionary = {chr(i): i for i in range(dictionary_size)}
 
     for ch in split_text:
-        print(string+ch)
-
-        if string+ch in dictionary:
-            string += ch
+        if (string + ch) in dictionary:
+            string = (string + ch)
         else:
-            result += string
-            temp = string + ch
-            dictionary.add(temp)
+            result.append(dictionary[string])
+            dictionary[(string + ch)] = dictionary_size
+            dictionary_size += 1
             string = ch
 
-    return result
+    if string:
+        result.append(dictionary[string])
+
+    return ','.join([str(i) for i in result])
 
 # takes text string and returns decompressed string
 def decompress(text):
-    # TODO
-    pass
+    split_text = [int(i) for i in text.split(',')]
+
+    result = ''
+    dictionary_size = 256
+    dictionary = {i: chr(i) for i in range(dictionary_size)}
+
+    prev = chr(split_text.pop(0))
+    result += prev
+    for curr in split_text:
+        if curr in dictionary:
+            entry = dictionary[curr]
+        elif curr == dictionary_size:
+            entry = prev + prev[0]
+        else:
+            raise Exception("error!")
+        result += entry
+
+        dictionary[dictionary_size] = prev + entry[0]
+        dictionary_size += 1
+
+        prev = entry
+    
+    return result
